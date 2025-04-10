55943c54c0230683f2d8b0c1cc2c6e8c57252dfc
Nikolay Chelpanov <nikch3008@mail.ru>
2025-04-10 20:32:03 +0700

    upd hw

--- my-first-react-appnpx/src/App.js
+++ my-first-react-appnpx/src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/Greeting';
-import CurrentTime from './components/CurrentTime';
-import EventCard from './components/EventCard';
 import Message from './components/Message';
 
 function App() {
-	return ( <div className = "App" >
-				<header className = "App-header" >
-					<Message title = "title" text = "text" / >
-					<h4> Random title </h4>
-					<p> Random text </p>
-					<Greeting / >
-					<CurrentTime / >
-					< EventCard title = "Группа 1"
-					date = "22 Октября"
-					location = "Москва" / >
-				</header>
-				<p> Hiden random text </p> 
-			</div>
+    return ( < div className = "App" >
+        		<header className = "App-header">
+        			<Message title = "Сообщение от тестера:" text = "Вы тестируете игру!" / >
+			<Message title = "Сообщение от тестера2:" text = "Вы победили!" / >
+        		</header> 
+	</div>
     );
 }
 
--- my-first-react-appnpx/src/components/Greeting.js
+++
@@ -1,9 +0,0 @@
-function Greeting() {
-    return ( <
-        div className = "greeting" >
-        <
-        h1 > Привет студенты < /h1> <
-        /div>
-    );
-}
-export default Greeting;
