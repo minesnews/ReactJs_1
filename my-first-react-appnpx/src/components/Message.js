557be2523399f9bcc3b4cbae57129283eb359188
Nikolay Chelpanov <nikch3008@mail.ru>
2025-05-01 11:16:28 +0700

    upd hw

--- my-first-react-appnpx/src/components/Message.js
+++ my-first-react-appnpx/src/components/Message.js
@@ -1,10 +1,11 @@
-function Message({title, text}) {
+import './message.css';
+
+function Message({ title, text }) {
     const currentTime = new Date;
-    return (
-        <div className="message">
-            <p className="message__title">{title}</p>
-            <p classTime="message_time">{ currentTime.toLocaleDateString([], { hour: "2-digit", minute: "2-digit" }) }</p>
-            <p className="message__text">{text}</p>
+    return ( <div className = "message" >
+        <p className = "message__title" > { title } < /p> 
+        <p classTime = "message_time" > { currentTime.toLocaleDateString([], { hour: "2-digit", minute: "2-digit" }) } < /p> 
+        <p className = "message__text" > { text } < /p> 
         </div>
     )
 }
